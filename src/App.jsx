deb620e548e4e97690a76b58ee9d638a924f5df8
Paula Fernandez Aranda <paulafa8@gmail.com>
2025-10-16 14:16:15 +0200

    variables css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import './components/styles/Variables.css';
+
 import './App.css'
 
 function App() {
