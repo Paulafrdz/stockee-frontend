b4d5f7c51261b79131505d4b57ee6d458a0c3d6b
Paula Fernandez Aranda <paulafa8@gmail.com>
2026-05-07 17:32:11 +1000

    fix file name

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import React from 'react';
-import "./components/styles/Variables.css";
+import "./components/styles/variables.css";
 import './App.css';
 import AppRouter from './router/AppRouter';
 import "./driver-theme.css"
